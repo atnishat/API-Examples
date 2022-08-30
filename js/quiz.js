7b4adcf471e9d1830db0e180e53ed2b63cd7458c
atnishat <nishat.mu47@gmail.com>
2022-08-30 22:15:40 +0600

    api codes

---
+++ js/quiz.js
@@ -0,0 +1,10 @@
+// const bondCode=` I am Fake James bond . My new code is: 00${7+1+2}`
+
+
+
+// const data={ result: [ {userName: {title:'Mr.',name:"sakib khan"} } ] }
+// const url = `https://www.themealdb.com/api/json/v1/1/search.php?s={food}` 
+
+
+// const array = { hobbies: ["dancing", "singing", "acting"] };
+// console.log(JSON.stringify(array));
